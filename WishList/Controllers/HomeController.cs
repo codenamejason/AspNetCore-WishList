9365085dfae702f0a630772a8debb1996226bcf3
Jaxcoder <codenamejason@outlook.com>
2019-07-05 11:36:45 -0400

    Fix typo.

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -12,13 +12,24 @@ namespace WishList.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
 
         public IActionResult Error()
         {
-            return View();
+            return View("Error");
         }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
